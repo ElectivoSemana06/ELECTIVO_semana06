ed6e87ebc5abc88fcccc61bb8b51440143f30992
Giancarlos Marquez Aico <32379183+gika1619@users.noreply.github.com>
2017-09-29 13:06:51 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+class Tarea{
+    
+    
+    
+    function numerodosmayor($n1,$n2){
+        
+        if($n1>$n2)
+         return $n1;
+        else 
+            return $n2;
+    }
+   
+    
+    function menorDosNumeros($n1,$n2){
+        
+        if($n1>$n2)
+            return $n2;
+        else
+        return $n1;  
+    }
+    
+    
+    
+    RICARDO SI PUEDES VER ESTO? XD 
+    
+    
+}
+    
+    
+ 
